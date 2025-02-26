7be44d0ff57c309220a31da01adfdfbdaf1947bc
felipecsousa2000 <132520533+felipecsousa2000@users.noreply.github.com>
2025-02-26 17:32:41 -0300

    teste-comando-editado

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1- Number
+/*
+console.log(typeof 2)
+console.log(typeof 3)
+console.log(typeof "felipe")
+console.log(typeof true)*/
+
+console.log(20);
+console.log(typeof 3) /*Number */
+console.log(typeof 15);/*Number */
+console.log(typeof  3);/*Number */
+console.log(typeof "felipe")/*String */
+console.log(typeof true); /*Booleano */
+console.log(typeof null)/*Object */
+
+console.log(20);
+
+console.log()
+
